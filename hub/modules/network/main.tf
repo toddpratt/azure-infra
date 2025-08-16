3594562d8b78f580d5dfdc0a5fa49260fea06227
Todd Pratt <tpratt@protonmail.com>
2025-08-16 19:56:41 +0000

    Fix a typo

--- hub/modules/network/main.tf
+++ hub/modules/network/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 variable "resource_group_name" { type = string }
 variable "location"            { type = string }
-variable "hub_network"         { type = string }
+variable "name_prefix"         { type = string }
 variable "vnet_cidr"           { type = string }
 variable "subnets"             { type = map(string) }
 
